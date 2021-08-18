4bffa46a9b71574e2d780a6f66ab5f9782b28e16
LumuSoft <lumusoft@gmail.com>
2021-08-18 11:23:29 +0400

    db sql

---
+++ database/deposystem (1).sql
@@ -0,0 +1,289 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1:3306
+-- Üretim Zamanı: 18 Ağu 2021, 10:21:59
+-- Sunucu sürümü: 8.0.12
+-- PHP Sürümü: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `deposystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `post_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `comments`
+--
+
+INSERT INTO `comments` (`id`, `post_id`, `user_id`, `content`, `created_at`, `updated_at`) VALUES
+(3, 49, 2, 'bahfhas dfh asdf asdjf asflasfd', '2021-08-09 17:02:38', '2021-08-09 17:02:38');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2019_08_19_000000_create_failed_jobs_table', 1),
+(3, '2021_08_07_133408_create_posts_table', 2),
+(4, '2021_08_07_134248_create_post_products_table', 2),
+(5, '2021_08_08_164831_create_voices_table', 2),
+(6, '2021_08_08_165653_create_comments_table', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `project_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `posts`
+--
+
+INSERT INTO `posts` (`id`, `project_name`, `user_id`, `status`, `created_at`, `updated_at`) VALUES
+(49, 'TEST PROJECT', 2, '0', '2021-08-09 05:40:31', '2021-08-09 17:22:02'),
+(50, 'TEST PROJECT', 2, '2', '2021-08-09 05:52:45', '2021-08-09 05:52:45');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `post_products`
+--
+
+CREATE TABLE `post_products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `post_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `destination` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `valley_of_measure` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quantity` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quantity_in_stock` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `instock` tinyint(1) DEFAULT NULL,
+  `Decision` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `post_products`
+--
+
+INSERT INTO `post_products` (`id`, `user_id`, `post_id`, `name`, `destination`, `valley_of_measure`, `quantity`, `quantity_in_stock`, `instock`, `Decision`, `image`) VALUES
+(20, 2, 49, 'ilk name', 'teyinati', 'cm', '23', NULL, NULL, NULL, 'uploads/product/images/49/1981628502031.jpg'),
+(21, 2, 49, 'material adi', 'teyinat2', 'kq', '500', NULL, NULL, NULL, 'uploads/product/images/49/6371628502031.jpg'),
+(22, 2, 50, 'ilk name', 'teyinati', 'cm', '23', NULL, NULL, NULL, 'uploads/product/images/50/7921628502765.jpg'),
+(23, 2, 50, 'material adi', 'teyinat2', 'kq', '500', NULL, NULL, NULL, 'uploads/product/images/50/1961628502765.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `department` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `role`, `department`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Rəis', 'demo@admin.com', '1', 'Rəis', NULL, '$2y$10$KG1T1VDZtoeZF1EmdUQR.uDBr0Cw1l11UkGfHLjIFSTU4jwYjID8e', NULL, '2021-08-04 10:41:30', '2021-08-04 10:41:30'),
+(2, 'Teymur \r\n Məmmədzadə\r\n\r\n', 'it@admin.com', '2', 'İT Department Center\r\n', NULL, '$2y$10$KG1T1VDZtoeZF1EmdUQR.uDBr0Cw1l11UkGfHLjIFSTU4jwYjID8e', NULL, '2021-08-04 10:41:30', '2021-08-04 10:41:30'),
+(3, 'Murad\r\n', 'supervisor@admin.com', '3', 'Nəzarətçi', NULL, '$2y$10$KG1T1VDZtoeZF1EmdUQR.uDBr0Cw1l11UkGfHLjIFSTU4jwYjID8e', NULL, '2021-08-04 10:41:30', '2021-08-04 10:41:30'),
+(4, 'Anbar', 'anbar@admin.com', '4', 'Depo', NULL, '$2y$10$KG1T1VDZtoeZF1EmdUQR.uDBr0Cw1l11UkGfHLjIFSTU4jwYjID8e', NULL, '2021-08-04 10:41:30', '2021-08-04 10:41:30'),
+(5, 'Anbar Köməkçisi', 'komekcianbar@admin.com', '5', 'Anbar Köməkçisi', NULL, '$2y$10$KG1T1VDZtoeZF1EmdUQR.uDBr0Cw1l11UkGfHLjIFSTU4jwYjID8e', NULL, '2021-08-04 10:41:30', '2021-08-04 10:41:30'),
+(6, 'Ayan x', 'ayan@admin.com', '6', 'Nəzarətçi', NULL, '$2y$10$KG1T1VDZtoeZF1EmdUQR.uDBr0Cw1l11UkGfHLjIFSTU4jwYjID8e', NULL, '2021-08-04 10:41:30', '2021-08-04 10:41:30'),
+(7, 'Təchizat', 'techizat@admin.com', '7', 'Təchizat', NULL, '$2y$10$KG1T1VDZtoeZF1EmdUQR.uDBr0Cw1l11UkGfHLjIFSTU4jwYjID8e', NULL, '2021-08-04 10:41:30', '2021-08-04 10:41:30');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `voices`
+--
+
+CREATE TABLE `voices` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `post_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `voice` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `voices`
+--
+
+INSERT INTO `voices` (`id`, `post_id`, `user_id`, `voice`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 'D:\\OSPanel\\domains\\deposystem\\public\\voices/1629204033801219357.wav', NULL, NULL);
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `post_products`
+--
+ALTER TABLE `post_products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- Tablo için indeksler `voices`
+--
+ALTER TABLE `voices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `post_products`
+--
+ALTER TABLE `post_products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `voices`
+--
+ALTER TABLE `voices`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
